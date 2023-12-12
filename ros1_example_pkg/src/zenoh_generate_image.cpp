bed9ad1d9efec7f120399247bcc01030cc0b319e
Lucas Walter <wsacul@gmail.com>
2023-12-12 15:45:05 -0800

    need to defrag and garbage collect shared memory to avoid Value exception (unless using the same sized message in it every time?)

--- ros1_example_pkg/src/zenoh_generate_image.cpp
+++ ros1_example_pkg/src/zenoh_generate_image.cpp
@@ -35,6 +35,9 @@ template <typename M> zenohc::Payload
   const auto length = ros::serialization::serializationLength(*msg);
   auto shmbuf = zenohc::expect<zenohc::Shmbuf>(z_manager_->alloc(length));
 
+  ROS_DEBUG_STREAM("msg len: " << length << " adr: " << static_cast<void*>(shmbuf.ptr())
+      << " len: " << shmbuf.get_length() << " cap: " << shmbuf.get_capacity());
+
   ros::serialization::OStream ostream(shmbuf.ptr(), length);
   ros::serialization::serialize(ostream, *msg);
 
@@ -64,22 +67,38 @@ void GenerateImage::update(const ros::TimerEvent& event)
   auto image_msg = cv_image.toImageMsg();
   image_msg->header.stamp = event.current_real;
   image_msg->encoding = "rgb8";
-  z_pub_.put_owned(std::move(ros_msg_to_payload(image_msg)), options);
 
-  ROS_INFO_STREAM_THROTTLE(2.0, "published '" << "Image"
-      << ", image bytes " << image_msg->data.size()
-      << " " << image_msg->width << "x" << image_msg->height);
+  auto float0_msg = std_msgs::Float64();
+  float0_msg.data = 0.12345689101112131415;
 
+  auto float1_msg = std_msgs::Float64();
+  float1_msg.data = 333.0333;
 
-  // TODO(lucasw) multiple calls to ros_msg_to_payload are causing the core dump,
-  // must be allocating wrong
-  auto float_msg = std_msgs::Float64();
-  float_msg.data = 0.12345689101112131415;
-  const auto payload0 = ros_msg_to_payload(boost::make_shared<std_msgs::Float64>(float_msg));
-  // test0_pub_.put_owned(std::move(payload0), options);
-  float_msg.data = 333.0333;
-  const auto payload1 = ros_msg_to_payload(boost::make_shared<std_msgs::Float64>(float_msg));
-  // test0_pub_.put_owned(std::move(payload1), options);
+  try {
+    auto image_payload = ros_msg_to_payload(image_msg);
+    z_pub_.put_owned(std::move(image_payload), options);
+    ROS_INFO_STREAM_THROTTLE(4.0, "published '" << "Image"
+        << ", image bytes " << image_msg->data.size()
+        << " " << image_msg->width << "x" << image_msg->height);
+
+    // TODO(lucasw) multiple calls to ros_msg_to_payload are causing the core dump,
+    // must be allocating wrong
+    auto payload0 = ros_msg_to_payload(boost::make_shared<std_msgs::Float64>(float0_msg));
+    test0_pub_.put_owned(std::move(payload0), options);
+
+    auto payload1 = ros_msg_to_payload(boost::make_shared<std_msgs::Float64>(float1_msg));
+    test1_pub_.put_owned(std::move(payload1), options);
+  } catch (zenohc::ErrorMessage& ex) {
+    ROS_WARN_STREAM(ex.as_string_view());
+  }
+
+  // this takes very little time but seems to be necessary to manage manually
+  const auto t0 = ros::WallTime::now();
+  const auto garbage_collected = z_manager_->gc();
+  const auto defragged = z_manager_->defrag();
+  const auto t1 = ros::WallTime::now();
+  ROS_INFO_STREAM_THROTTLE(4.0, (t1 - t0).toSec() << "s defragged: " << defragged
+      << " garbage collected: " << garbage_collected);
 }
 
 int main(int argc, char* argv[])
@@ -95,7 +114,7 @@ int main(int argc, char* argv[])
   oss << z_session.info_zid();
   // May have to make this larger depending on bouncing ball width and height
   auto z_manager = zenohc::expect<zenohc::ShmManager>(shm_manager_new(z_session, oss.str().c_str(),
-                                                      2048 * 1024 * 3 * 4));
+                                                      2048 * 1024 * 3 * 10));
 
   GenerateImage generate_image(&z_session, &z_manager);
   ros::spin();

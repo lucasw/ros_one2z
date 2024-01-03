20469398b7fcc3a5236125c85faf3d7e53ccde97
Lucas Walter <wsacul@gmail.com>
2024-01-02 17:48:33 -0800

    experimenting with serializing a message, will adapt to a ros2msg+zenoh example

--- ros2_example_cpp/src/minimal_publisher.cpp
+++ ros2_example_cpp/src/minimal_publisher.cpp
@@ -10,7 +10,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <std_msgs/msg/int8.hpp>
+// #include <std_msgs/msg/int8.hpp>
 
 using namespace std::chrono_literals;
 
@@ -22,7 +22,7 @@ class MinimalPublisher : public rclcpp::Node
     {
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+          500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
 
   private:
@@ -31,6 +31,28 @@ class MinimalPublisher : public rclcpp::Node
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+
+      // serialization experiment
+      {
+        rclcpp::Serialization<std_msgs::msg::String> stringSerializer;
+        rclcpp::SerializedMessage serialized_msg;
+        stringSerializer.serialize_message(&message, &serialized_msg);
+        const auto buffer_begin = serialized_msg.get_rcl_serialized_message().buffer;
+        const auto buffer_end = buffer_begin + serialized_msg.size();
+        std::string serialized_msg_str(buffer_begin, buffer_end);
+        std::cout << "'" << serialized_msg_str << "' " <<  serialized_msg.size() << "\n";
+        std::cout << "buffer: ";
+        for (size_t i = 0; i < serialized_msg.size(); ++i) {
+          std::cout << "[" << int(buffer_begin[i]) << "] ";
+        }
+        std::cout << "\n";
+        std::cout << "str:    ";
+        for (size_t i = 0; i < serialized_msg_str.size(); ++i) {
+          std::cout << "[" << int(serialized_msg_str[i]) << "] ";
+        }
+        std::cout << "\n";
+      }
+
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
